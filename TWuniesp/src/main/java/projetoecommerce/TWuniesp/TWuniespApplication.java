d412b38953eebc9384bf165248b7448184439293
itzLukeAll <lucasbadue50@gmail.com>
2023-04-04 23:22:42 -0300

    Usuario

---
+++ TWuniesp/src/main/java/projetoecommerce/TWuniesp/TWuniespApplication.java
@@ -0,0 +1,13 @@
+package projetoecommerce.TWuniesp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TWuniespApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TWuniespApplication.class, args);
+	}
+
+}
---
+++ TWuniesp/src/test/java/projetoecommerce/TWuniesp/TWuniespApplicationTests.java
@@ -0,0 +1,13 @@
+package projetoecommerce.TWuniesp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TWuniespApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
